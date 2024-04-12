f4ad277812a35d180ed5819cc8d6798d7bdaa562
root-Brainoverflow <root@brainoverflow.kr>
2024-04-12 16:50:09 +0900

    4.12
    .

--- app/main.py
+++ app/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
 @app.get("/")
 def root():
     return { "Hello": "World" }
@@ -10,3 +15,7 @@ def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
