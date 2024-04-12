21a49373ca559976cce95ccd621ca84f8fa1a763
root-Brainoverflow <root@brainoverflow.kr>
2024-04-12 17:28:53 +0900

    4.12
    .

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -8,8 +6,9 @@ app = FastAPI()
 user_name = None
 
 @app.post("/user")
-def create_item(user: user_name):
-    user_name = user.name
+def create_item(new_name):
+    global user_name
+    user_name = new_name
     return { "message": f"Your name is {user_name}!"}
 
 @app.get("/user")
@@ -17,8 +16,9 @@ def get_item():
     return {"message": f"Hey, your name is {user_name}!"}
 
 @app.put("/user")
-def update_item(user: user_name):
-    user_name = user.name
+def update_item(update_name):
+    global user_name
+    user_name = update_name
     return {"message": f"Your name is now {user_name}!"}
 
 @app.delete("/user")
