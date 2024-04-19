2dacfe3f60749cb5993901fcc630946f54c801d1
root-Brainoverflow <root@brainoverflow.kr>
2024-04-19 15:45:53 +0900

    04.19

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ user_name = None
 
 @app.get("/")
 def root():
-    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2023&month=12&metroCd=11&cityCd=12&apiKey=FaQvGnHYNQ7GSW7371cu3E6McUG3sF43OxzIKs09&returnType=json"
+    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=12&metroCd=11&cityCd=12&apiKey=FaQvGnHYNQ7GSW7371cu3E6McUG3sF43OxzIKs09&returnType=json"
     
     contents = requests.get(URL).text
 
